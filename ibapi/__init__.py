6e8a415852dd1d2f5b2a92bc3730c7f8651163ae
Jin Miao <jinmiao@utexas.edu>
2020-06-13 15:40:36 -0700

    revert to orig

--- ibapi/__init__.py
+++ ibapi/__init__.py
@@ -12,8 +12,7 @@ VERSION = {
 
 
 def get_version_string():
-    # version = '{major}.{minor}.{micro}'.format(**VERSION)
-    version = '9.79.01'
+    version = '{major}.{minor}.{micro}'.format(**VERSION)
     return version
 
 __version__ = get_version_string()
